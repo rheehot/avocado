f530355387b3d3510d0103f90abed80d8e874c12
rr_inhye <inhye1028@gmail.com>
2020-12-03 02:37:38 +0900

    main controller

---
+++ src/main/java/com/iw/avo/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.iw.avo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@AllArgsConstructor
+public class MainController {
+	@RequestMapping("/")
+	public String home(Model model) {
+		log.info("Welcome to avocado home!");
+		
+		return "main/index";
+	}
+}
